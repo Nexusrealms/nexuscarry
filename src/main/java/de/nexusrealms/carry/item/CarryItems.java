f23f3657b6e037963a83f1a6d701e911cb1d3d48
farpo <farpoproject@gmail.com>
2025-08-12 14:26:13 +0200

    color trial and error

--- src/main/java/de/nexusrealms/carry/item/CarryItems.java
+++ src/main/java/de/nexusrealms/carry/item/CarryItems.java
@@ -22,7 +22,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class CarryItems {
-    public static final Item SATCHEL = create("satchel", BagItem::new, new Item.Settings().maxCount(1).component(DataComponentTypes.DYED_COLOR, new DyedColorComponent(0xffa993)).component(Components.BAG_STRAP_COLOR, new DyedColorComponent(0xffa8a8)), ItemGroups.INGREDIENTS);
+    public static final Item SATCHEL = create("satchel", BagItem::new, new Item.Settings().maxCount(1).component(DataComponentTypes.DYED_COLOR, new DyedColorComponent(0xffcba4)).component(Components.BAG_STRAP_COLOR, new DyedColorComponent(0xffa8a8)), ItemGroups.INGREDIENTS);
     private static <T extends Item> T create(String name, Function<Item.Settings, T> constructor, Item.Settings settings, RegistryKey<ItemGroup> itemGroup){
         return create(name, constructor, settings, itemGroup, FabricItemGroupEntries::add);
     }
