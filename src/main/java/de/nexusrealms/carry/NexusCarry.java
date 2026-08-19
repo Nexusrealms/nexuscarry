c9f5598829473ba62716aa21f09f03853b3f6f17
farpo <farpoproject@gmail.com>
2026-08-19 17:25:35 +0200

    Add /bagsee

--- src/main/java/de/nexusrealms/carry/NexusCarry.java
+++ src/main/java/de/nexusrealms/carry/NexusCarry.java
@@ -7,29 +7,42 @@ import de.nexusrealms.carry.item.CarryItems;
 import de.nexusrealms.carry.network.CarryPackets;
 import de.nexusrealms.carry.recipe.CarryRecipes;
 import de.nexusrealms.dipdye.api.CauldronDipApi;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketsApi;
 import dev.emi.trinkets.api.event.TrinketEquipCallback;
 import dev.emi.trinkets.api.event.TrinketUnequipCallback;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.command.DefaultPermissions;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.permission.PermissionCheck;
 import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ContainerComponent;
 import net.minecraft.component.type.DyedColorComponent;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
 import net.minecraft.resource.featuretoggle.FeatureSet;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
 import net.minecraft.world.rule.GameRule;
 import net.minecraft.world.rule.GameRuleCategory;
 import net.minecraft.world.rule.GameRuleType;
 import net.minecraft.world.rule.GameRuleVisitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class NexusCarry implements ModInitializer {
 	public static final String MOD_ID = "nexuscarry";
@@ -50,7 +63,20 @@ public class NexusCarry implements ModInitializer {
 		CarryItems.init();
 		CarryPackets.init();
 		CarryRecipes.init();
-
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal("bagsee")
+				.then(CommandManager.argument("player", EntityArgumentType.player())
+						.requires(CommandManager.requirePermissionLevel(new PermissionCheck.Require(DefaultPermissions.MODERATORS)))
+						.requires(ServerCommandSource::isExecutedByPlayer)
+						.executes(context -> {
+							ServerPlayerEntity player = EntityArgumentType.getPlayer(context, "player");
+							List<Pair<SlotReference, ItemStack>> list = player.getComponent(TrinketsApi.TRINKET_COMPONENT)
+									.getEquipped(stack -> stack.getItem() instanceof BagItem);
+							list.stream()
+									.map(Pair::getRight)
+									.findFirst()
+									.ifPresentOrElse(stack -> ((BagItem) stack.getItem()).openScreen(context.getSource().getPlayer(), stack), () -> context.getSource().sendError(player.getName().copy().append(" has no bag")));
+							return 1;
+						}))));
 		LOGGER.info("Hello Fabric world!");
 		if(FabricLoader.getInstance().isModLoaded("dip-dye")) DipDyeCompat.init();
 	}
